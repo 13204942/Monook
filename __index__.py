110b6bfae363aa761faea6c7a835aa6398014b65
Fangyijie Wang <fangyijie.wang@fleetmatics.com>
2018-05-23 12:15:37 +0100

    Add About page and reuse header templates

--- __index__.py
+++ __index__.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def homepage():
     return render_template("main.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
 
 @app.route('/newnote/')
 def newnote():
